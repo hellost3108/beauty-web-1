8db6ea771ee8f0c6a0d2b617e95260cdb82af005
Thai Dang <dangsinhthai3108@gmail.com>
2026-08-13 17:36:04 +0700

    Việt hóa nội dung trang Shop và cập nhật ảnh sản phẩm/hero
    Thay ảnh placeholder bằng ảnh thật (shop-hero.png, sanpham400x500.png), dịch review khách hàng và nhãn danh mục sang tiếng Việt, đổi giá sang định dạng VND.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned/// <reference types="next" />
+/// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/types/routes.d.ts";
 
