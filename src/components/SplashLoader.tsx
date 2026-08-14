58e451bf7c8eb0fe3249856a3368ac2168676105
Tuấn Cường <tuancuong@Mac.lan>
2026-08-14 14:10:12 +0700

    Change splash accent color to deep teal (#1B5E5E) per feedback

--- src/components/SplashLoader.tsx
+++ src/components/SplashLoader.tsx
@@ -76,7 +76,7 @@ const SplashLoader: React.FC<SplashLoaderProps> = ({ onComplete }) => {
                     {/* Circle Border - Drops in then Fades out slowly during pull */}
                     <div
                         className={cn(
-                            "absolute inset-0 border-[3px] border-[#A65D2E] rounded-full transition-all duration-700 ease-out",
+                            "absolute inset-0 border-[3px] border-[#1B5E5E] rounded-full transition-all duration-700 ease-out",
                             stage === 'initial' || stage === 'text' ? "-translate-y-full opacity-0" :
                                 (stage === 'pull' || stage === 'finish') ? "translate-y-0 opacity-0" : "translate-y-0 opacity-100"
                         )}
@@ -95,7 +95,7 @@ const SplashLoader: React.FC<SplashLoaderProps> = ({ onComplete }) => {
                     {/* Vertical Line - Attached to bottom of circle */}
                     <div
                         className={cn(
-                            "absolute top-full left-1/2 -translate-x-1/2 w-[3px] bg-[#A65D2E] origin-top transition-all ease-in-out",
+                            "absolute top-full left-1/2 -translate-x-1/2 w-[3px] bg-[#1B5E5E] origin-top transition-all ease-in-out",
                             stage === 'line' ? "h-[50vh]" :
                                 stage === 'pull' ? "h-[50vh] opacity-0" : "h-0 opacity-0"
                         )}
